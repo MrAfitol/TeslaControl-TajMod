7e229054c3c26a5ade38ece4dea089e4ca812969
MrAfitol <76150070+MrAfitol@users.noreply.github.com>
2026-04-03 03:07:38 +0200

    Upload the project

---
+++ TeslaControl/Patches/TeslaGatePlayersInRangePatch.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+
+namespace TeslaControl.Patches
+{
+    [HarmonyPatch(typeof(TeslaGate), nameof(TeslaGate.PlayersInRange))]
+    public class TeslaGatePlayersInRangePatch
+    {
+        private static void Postfix(ref PlayerStats[] __result)
+        {
+            if (__result == null || __result.Length == 0)
+                return;
+
+            var ignoreRoles = Plugin.Instance.GetAllowedRoles();
+            var ignoreCuffedSetting = Plugin.Instance.IsTeslaIgnoreCuffedPlayers.Value;
+
+            PlayerStats[] original = __result;
+            List<PlayerStats> newResult = new List<PlayerStats>(original.Length);
+            for (int i = 0; i < original.Length; i++)
+            {
+                PlayerStats player = original[i];
+                if (!ignoreRoles.Contains(player.ccm.curClass) && (ignoreCuffedSetting && !player.hub.playerInteract.IsCuffed()))
+                    newResult.Add(player);
+            }
+
+            __result = newResult.ToArray();
+        }
+    }
+}
---
+++ TeslaControl/Plugin.cs
@@ -0,0 +1,52 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TeslaControl
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Plugin Instance { get; private set; }
+
+        public ConfigEntry<string> TeslaIgnoreRoles;
+        public ConfigEntry<bool> IsTeslaIgnoreCuffedPlayers;
+
+        private static Harmony Harmony;
+
+        public void Awake()
+        {
+            Instance = this;
+
+            TeslaIgnoreRoles = Config.Bind("General", "TeslaIgnoreRoles",
+                "Scientist, FacilityGuard, NtfCadet, NtfLieutenant, NtfCommander",
+                "Roles that the Tesla gate should ignore.");
+
+            IsTeslaIgnoreCuffedPlayers = Config.Bind("General", "IsTeslaIgnoreCuffedPlayers", true,
+                "If true, Tesla gates will ignore cuffed (handcuffed) players and won't trigger on them.");
+
+            Harmony = new Harmony("tajmod.tesla-control");
+            Harmony.PatchAll();
+        }
+
+        public void OnDestroy()
+        {
+            Harmony.UnpatchSelf();
+            Harmony = null;
+            Instance = null;
+        }
+
+        public List<RoleType> GetAllowedRoles()
+        {
+            return TeslaIgnoreRoles.Value
+                .Split(',')
+                .Select(r => Enum.TryParse<RoleType>(r, true, out var role) ? role : (RoleType?)null)
+                .Where(r => r.HasValue)
+                .Select(r => r.Value)
+                .ToList();
+        }
+    }
+}
